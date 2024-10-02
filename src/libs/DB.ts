825d751e1ced5d9446aef929c8887dc9399da6eb
Alazar Adane <139617747+alazaradane@users.noreply.github.com>
2024-10-02 20:00:12 +0300

    Initialize vercel-button

---
+++ src/libs/DB.ts
@@ -0,0 +1,16 @@
+import { createClient } from '@libsql/client';
+import { drizzle } from 'drizzle-orm/libsql';
+import { migrate } from 'drizzle-orm/libsql/migrator';
+
+import { Env } from './Env.mjs';
+
+const client = createClient({
+  url: Env.DATABASE_URL,
+  authToken: Env.DATABASE_AUTH_TOKEN,
+});
+
+export const db = drizzle(client);
+
+if (process.env.NODE_ENV !== 'production') {
+  await migrate(db, { migrationsFolder: './migrations' });
+}
