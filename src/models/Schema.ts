e843b098d1e7c73c1288122af87c3327e6a6257d
Alazar Adane <139617747+alazaradane@users.noreply.github.com>
2024-10-02 20:01:24 +0300

    Initialize update-readme

---
+++ src/models/Schema.ts
@@ -0,0 +1,11 @@
+import { integer, pgTable, serial, timestamp } from 'drizzle-orm/pg-core';
+
+export const counterSchema = pgTable('counter', {
+  id: serial('id').primaryKey(),
+  count: integer('count').default(0),
+  updatedAt: timestamp('updated_at', { mode: 'date' })
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  createdAt: timestamp('created_at', { mode: 'date' }).defaultNow().notNull(),
+});
