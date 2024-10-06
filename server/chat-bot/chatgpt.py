b74d3338a57edb21808e336f29dd98f1d5c372f8
justsoul12 <fetih.girma1@gmail.com>
2024-10-06 05:29:50 +0100

    sunday morning chat-bot

---
+++ server/chat-bot/chatgpt.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import constants
+import openai
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import DirectoryLoader, TextLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.indexes.vectorstore import VectorStoreIndexWrapper
+from langchain.llms import OpenAI
+from langchain.vectorstores import Chroma
+
+os.environ["OPENAI_API_KEY"]= constants.API_KEY
+
+query= sys.argv[1]
+
+print(query)
